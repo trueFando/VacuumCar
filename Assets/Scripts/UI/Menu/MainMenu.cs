b876d5a65a442cab0491359f53761abd7fa68083
Alex Kravtsov <a.kravcov@softmg.ru>
2025-08-29 17:55:24 +0300

    Menu.

---
+++ Assets/Scripts/UI/Menu/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI.Menu
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void Play()
+        {
+            SceneManager.LoadScene("Level1");
+        }
+    }
+}
